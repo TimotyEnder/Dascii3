2152b398ce84abed134c4bd9363e29b98a1feb1e
TimotyEnder <timotyender@gmail.com>
2026-05-14 19:36:00 +0200

    we be rotating

--- src/model/objects/line.rs
+++ src/model/objects/line.rs
@@ -1,3 +1,5 @@
+use std::cmp::{Ordering, max};
+
 use crate::{
     model::elements::pos3::{self, Pos3},
     screenspace::{
@@ -73,18 +75,57 @@ impl Line {
             &(self.from.z() + self.to.z() / 2.0),
         )
     }
+    // literally just https://en.wikipedia.org/wiki/Bresenham's_line_algorithm
+    fn bresenham_line_algorithm(
+        &self,
+        from: &ScreenPosition,
+        to: &ScreenPosition,
+        screen: &mut Screen,
+    ) {
+        let dx = (to.x() as isize - from.x() as isize).abs(); //total x distance
+        let dy = (to.y() as isize - from.y() as isize).abs(); //total y distance
+        let sx = if to.x() >= from.x() { 1 } else { -1 }; //step for x
+        let sy = if to.y() >= from.y() { 1 } else { -1 }; //step for y
+        let mut err = dx - dy; //deviation from mathematical line and actual pixel position, decides next movement
+        let mut x = from.x() as isize;
+        let mut y = from.y() as isize;
+
+        loop {
+            screen.color_cell(
+                &ScreenPosition::with_pos(&(x as usize), &(y as usize)),
+                &self.color,
+            );
+            if x == to.x() as isize && y == to.y() as isize {
+                break;
+            }
+            //if 2*err > -dy, then take x step
+            //if 2*err < dx, then take y step
+            let e2 = 2 * err; //avoids fractions and enables the use of integer maths
+            if e2 > -dy {
+                //above the y of the current line so go forward
+                err -= dy;
+                x += sx;
+            }
+            if e2 < dx {
+                //in front of the x  so go up to be on the same height
+                err += dx;
+                y += sy;
+            }
+        }
+    }
 }
 impl Drawable for Line {
     fn draw(&self, screen: &mut Screen) {
         let from_pos: ScreenPosition = screen.project_point(&self.from);
         let to_pos: ScreenPosition = screen.project_point(&self.to);
-        screen.color_cell(&from_pos, &self.color);
-        let mut cur_pos = from_pos;
-        while (cur_pos != to_pos) {
-            let (x_dir, y_dir) = Self::distanceVector(&cur_pos, &to_pos);
-            Self::move_pos_towards_dir(&x_dir, &y_dir, &mut cur_pos);
-            screen.color_cell(&cur_pos, &self.color);
-        }
+        // screen.color_cell(&from_pos, &self.color);
+        // let mut cur_pos = from_pos;
+        // while (cur_pos != to_pos) {
+        //     let (x_dir, y_dir) = Self::distanceVector(&cur_pos, &to_pos);
+        //     Self::move_pos_towards_dir(&x_dir, &y_dir, &mut cur_pos);
+        //     screen.color_cell(&cur_pos, &self.color);
+        // }
+        self.bresenham_line_algorithm(&from_pos, &to_pos, screen);
     }
 
     fn position(&self) -> Pos3 {
